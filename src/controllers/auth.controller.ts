df2cbc133d78aa78794ece0b2b4a53f02ea77fb0
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-06-23 14:49:45 -0300

    renew token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -129,3 +129,63 @@ export const logoutAll = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const renewToken = async (req: Request, res: Response) => {
+	const token =
+		process.env.USE_COOKIES === 'true'
+			? req.cookies['x-token']
+			: req.header('x-token');
+
+	if (!token) {
+		return res.status(400).json({
+			errors: [{ msg: GeneralErrorCodes.SessionNotFound }],
+		});
+	}
+
+	const { sub } = jwt.decode(token) as TokenPayload;
+
+	if (!sub) {
+		return res.status(400).json({
+			errors: [{ msg: GeneralErrorCodes.SessionNotFound }],
+		});
+	}
+
+	try {
+		const newToken = await generateJWT(sub);
+		const session = await Session.findOne({ _id: sub })
+			.select('userRef')
+			.populate('userRef');
+		const user = session?.userRef;
+		const { username, img } = user! as any;
+
+		const useCookies = process.env.USE_COOKIES === 'true';
+		if (useCookies) {
+			res.cookie('x-token', newToken, {
+				httpOnly: true,
+				secure: false,
+				sameSite: 'none',
+			});
+			res.status(200).json({
+				ok: true,
+				user: {
+					username,
+					img,
+				},
+			});
+		} else {
+			return res.status(200).json({
+				ok: true,
+				user: {
+					username,
+					img,
+				},
+				token: newToken,
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({
+			errors: [{ msg: GeneralErrorCodes.InternalServerError }],
+		});
+	}
+};
