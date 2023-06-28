2b0df52338e66d53bc8d0ba3db94bc73b2579903
PieroSaucedo <piero.saucedo@correo.ucu.edu.uy>
2023-06-28 20:24:50 -0300

    Comments Endpoints Protection Added

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -7,15 +7,32 @@ import {
 } from '../controllers/comment.controller';
 import { validateJWT } from '../middlewares/validate-JWT';
 import { validateSession } from '../middlewares/validate-session';
+import {check} from "express-validator";
+import {CommentErrorCodes, UserErrorCodes} from "../helpers";
 
 export const router = Router();
 
 router.get('/', [validateSession, validateJWT], getComments);
 
-router.post('/', [validateSession, validateJWT], createComment);
+router.post('/', [validateSession, validateJWT,
+	check('creator', 'Id is not valid')
+		.isMongoId()
+		.withMessage(UserErrorCodes.UserIdInvalid),
+	check('content', 'Content is required')
+		.not()
+		.isEmpty()
+		.withMessage(CommentErrorCodes.CommentContentRequired)
+	],
+	createComment);
 
 router.delete('/:id', [validateSession, validateJWT], deleteComment);
 
-router.put('/:id', [validateSession, validateJWT], updateComment);
+router.put('/:id', [validateSession, validateJWT,
+	check('content', 'Content is required')
+		.not()
+		.isEmpty()
+		.withMessage(CommentErrorCodes.CommentContentRequired),
+	],
+	updateComment);
 
 export default router;
